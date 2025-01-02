2291a91143aa09027ae63fa8741823b4890d8ef1
fmway <fm18lv@gmail.com>
2025-01-02 07:24:54 +0700

    fix: ...

--- modules/_shared/data.nix
+++ modules/_shared/data.nix
@@ -1,12 +1,15 @@
-{ config, lib, ... }: let
+{ config, lib, nixosConfig, ... } @ var: let
   inherit (lib)
     mkOption
     types
     ;
+  isHomeManager = var ? osConfig;
 in {
   options.data = mkOption {
     type = types.attrs;
     default = {};
   };
-  config = {};
+  config = lib.mkIf isHomeManager {
+    data = lib.mkBefore nixosConfig.data;
+  };
 }
