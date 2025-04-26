4cea1be78e6929523c2eaf87e0bbbc5fa12bdb69
fmway <fm18lv@gmail.com>
2025-04-27 06:36:56 +0700

    chore: genModules expose self result

--- src/__util/for-flake.nix
+++ src/__util/for-flake.nix
@@ -1,36 +1,37 @@
 { lib, root, ... }:
 {
-  genModules = moduleDir: args: lib.pipe moduleDir [
-    builtins.readDir
-    (lib.filterAttrs (_: v: v == "directory"))
-    (lib.attrNames)
-    (map (x: let
-      name = "${root.toCamelCase x}Modules";
-    in {
-      inherit name;
-      value = let
-        res = args: lib.pipe "${moduleDir}/${x}" [
-          (builtins.readDir)
-          (lib.filterAttrs (n: t: ! isNull (builtins.match ".+[.]nix" n) && t == "regular"))
-          (lib.attrNames)
-          (map (y: let
-            path = "${moduleDir}/${x}/${y}";
-          in {
-            name = lib.removeSuffix ".nix" y;
-            value = root.withImport path args;
-          }))
-          (lib.listToAttrs)
-        ];
-      in if name == "SharedModules" then
-        res
-      else res args;
-    }))
-    (lib.listToAttrs)
-    (x: let
-      gen = lib.listToAttrs (map (name: {
+  genModules = moduleDir: args: let
+    re = lib.pipe moduleDir [
+      builtins.readDir
+      (lib.filterAttrs (_: v: v == "directory"))
+      (lib.attrNames)
+      (map (x: let
+        name = "${root.toCamelCase x}Modules";
+      in {
         inherit name;
-        value = x.SharedModules (args // { inherit name; }) // (x.${name} or {});
-      }) [ "nixosModules" "darwinModules" "homeManagerModules" ]);
-    in removeAttrs x [ "SharedModules" ] // lib.optionalAttrs (x ? SharedModules) gen)
-  ];
+        value = let
+          res = args: lib.pipe "${moduleDir}/${x}" [
+            (builtins.readDir)
+            (lib.filterAttrs (n: t: ! isNull (builtins.match ".+[.]nix" n) && t == "regular"))
+            (lib.attrNames)
+            (map (y: let
+              path = "${moduleDir}/${x}/${y}";
+            in {
+              name = lib.removeSuffix ".nix" y;
+              value = root.withImport path args;
+            }))
+            (lib.listToAttrs)
+          ];
+        in if name == "SharedModules" then
+          res
+        else res (final // args);
+      }))
+      (lib.listToAttrs)
+    ];
+    gen = lib.listToAttrs (map (name: {
+      inherit name;
+      value = re.SharedModules (final // args // { inherit name; }) // (re.${name} or {});
+    }) [ "nixosModules" "darwinModules" "homeManagerModules" ]);
+    final = removeAttrs re [ "SharedModules" ] // lib.optionalAttrs (re ? SharedModules) gen;
+  in final;
 }
