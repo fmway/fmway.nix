b1681040ec6147913a903d640eb512964461e831
fmway <fm18lv@gmail.com>
2025-04-26 07:55:57 +0700

    chore(flake-parts): expose lib

--- src/__util/wrap.nix
+++ src/__util/wrap.nix
@@ -3,20 +3,32 @@
 in {
   mkFlake = { inputs, ... } @ v1: let
     inherit (inputs) flake-parts;
+    overlay-lib = self: super: (v1.specialArgs.lib or {}) // {
+      fmway = root // {
+        getInput = x: inputs.${x} or selfInputs.${x};
+      };
+      flake-parts = flake-parts.lib;
+    };
     arg1 = v1 // {
       specialArgs = (v1.specialArgs or {}) // {
         lib =
-          (v1.specialArgs.lib or {}) //
-          (inputs.nixpkgs or selfInputs.nixpkgs).lib.extend (self: super: {
-            fmway = root // {
-              getInput = x: inputs.${x} or selfInputs.${x};
-            };
-            flake-parts = flake-parts.lib;
-          });
+          (inputs.nixpkgs or selfInputs.nixpkgs).lib.extend overlay-lib;
       };
     };
   in arg2: flake-parts.lib.mkFlake arg1 ({ lib, ... }: {
     debug = lib.mkDefault true;
-    imports = [ arg2 ];
+    imports = [
+      arg2
+      selfInputs.self.flakeModules.nixpkgs
+      {
+        perSystem = { ... }: {
+          nixpkgs.overlays = [
+            (self: super: {
+              lib = super.lib.extend overlay-lib;
+            })
+          ];
+        };
+      }
+    ];
   });
 }
