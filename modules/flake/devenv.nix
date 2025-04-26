37269f2fd961f1575beeee6bd48b0b1522e90fce
fmway <fm18lv@gmail.com>
2025-04-26 13:34:14 +0700

    chore(flake-parts): fix devenv script, no more wait to kompil

--- modules/flake/devenv.nix
+++ modules/flake/devenv.nix
@@ -2,22 +2,52 @@
   superInputs = inputs;
 in { flake-parts-lib, lib, inputs, ... }: let
   devenv = inputs.devenv or superInputs.devenv;
+  mkScript = { config, pkgs, ... }: let
+    inherit (config) procfileScript test;
+    up =
+      if procfileScript.name == "devenv-up" then
+        "exec ${procfileScript} $@"
+      else
+        ''echo "No 'processes' option defined: https://devenv.sh/processes/" && exit 1'';
+    default = lib.fileContents (
+      lib.getExe (
+        lib.elemAt (
+          lib.filter (x: x.name == "devenv")
+        (config.packages)) 0
+      )
+    );
+    replaces = {
+      "#!/usr/bin/env bash" = "#!${lib.getExe pkgs.bash}";
+      "up)" = "up) ${up};";
+      "test)" = "test) exec ${test};";
+    };
+  in pkgs.writeScriptBin "devenv"
+    (lib.replaceStrings
+      (lib.attrNames replaces)
+      (lib.attrValues replaces)
+    default);
 in {
   imports = [
     devenv.flakeModule
   ];
   options.perSystem = flake-parts-lib.mkPerSystemOption ({ config, pkgs, system, ... }: {
     imports = [
-      ({ config, ... }: {
+      ({ config, pkgs, ... }: {
         config = lib.mkIf (config.devenv.shells != {}) {
-          devShells = let
-            # for lsp / completions
-            shells = lib.mapAttrs (name: devenv: devenv.shell // { 
+          devShells = lib.mapAttrs (name: devenv: let
+            shell = devenv.shell.overrideAttrs (old: {
+              nativeBuildInputs =
+                lib.filter
+                  (x: x.name != "devenv")
+                  (old.nativeBuildInputs or []) ++ [
+                (mkScript { inherit pkgs; config = devenv; })
+              ];
+            }) // { 
               config = config.devenv.shells.${name};
-              inherit (shells.${name}.config.outputs) options;
-              inherit (shells.${name}.options._module.args.value) pkgs;
-            }) config.devenv.shells;
-          in lib.mkForce shells;
+              inherit (shell.config.outputs) options;
+              inherit (shell.options._module.args.value) pkgs;
+            };
+          in lib.mkForce shell) config.devenv.shells;
         };
       })
     ];
