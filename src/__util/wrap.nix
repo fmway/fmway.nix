0bbf9685fe7ff9fa75107f8859ec941c524ccc57
fmway <fm18lv@gmail.com>
2025-04-26 19:44:01 +0700

    chore(flake-parts): overlay lib

--- src/__util/wrap.nix
+++ src/__util/wrap.nix
@@ -3,16 +3,30 @@
 in {
   mkFlake = { inputs, ... } @ v1: let
     inherit (inputs) flake-parts;
-    overlay-lib = self: super: (v1.specialArgs.lib or {}) // {
-      fmway = root // {
-        getInput = x: inputs.${x} or selfInputs.${x};
-      };
-      flake-parts = flake-parts.lib;
-    };
+    inherit (inputs.nixpkgs or selfInputs.nixpkgs) lib;
+    overlay = lib: x:
+      if lib.isAttrs x then
+        lib.extend (_: _: x)
+      else if lib.isFunction x then
+        lib.extend x
+      else if lib.isList x then
+        lib.foldl' overlay lib x
+      else throw "lib overlay doesn't support ${builtins.typeOf x}"
+    ;
+    overlay-lib = let
+      default = v1.specialArgs.lib or {};
+    in [
+      {
+        fmway = root // {
+          getInput = x: inputs.${x} or selfInputs.${x};
+        };
+        inherit (selfInputs.self) infuse;
+        flake-parts = flake-parts.lib;
+      }
+    ] ++ lib.flatten [ default ];
     arg1 = v1 // {
       specialArgs = (v1.specialArgs or {}) // {
-        lib =
-          (inputs.nixpkgs or selfInputs.nixpkgs).lib.extend overlay-lib;
+        lib = overlay lib overlay-lib;
       };
     };
   in arg2: flake-parts.lib.mkFlake arg1 ({ lib, ... }: {
@@ -24,7 +38,7 @@ in {
         perSystem = { ... }: {
           nixpkgs.overlays = [
             (self: super: {
-              lib = super.lib.extend overlay-lib;
+              lib = overlay super.lib overlay-lib;
             })
           ];
         };
