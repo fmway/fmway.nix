cb4a53c7f3f5fe82aa35717619dc50a82a21c4e8
fmway <fm18lv@gmail.com>
2025-06-17 21:42:58 +0700

    chore(util/mkParse): add debug, fix close postfix problem

--- lib/fmway/parser.nix
+++ lib/fmway/parser.nix
@@ -18,17 +18,43 @@
   fixedInMatch = str:
     lib.foldl' (acc: curr: acc + (if lib.any (x: curr == x) listNeedFixed then "[${curr}]" else curr)) "" (lib.splitString "" str);
 
-  mkParse = experimental: { prefix ? "{{", postfix ? "}}", ... } @ variables: str: let
+  # for handle ctx multiple postfix
+  getCtx = str: postfix: let
+    fn = str: let
+      matches = lib.match "^(.*)${fixedInMatch postfix}(.*)$" str;
+      h = lib.head matches;
+      t = lib.last matches;
+      h'= fn h;
+    in if isNull matches then str else {
+      pre = if lib.isString h' then h' else fn h'.pre;
+      post= lib.optionalString (!lib.isString h') h'.post + t + postfix;
+    };
+    res = fn str;
+  in rec {
+    pre' = if lib.isString res then res else res.pre;
+    pre  = lib.trim pre';
+    post'= if lib.isString res then "" else res.post;
+    post = lib.trim post';
+  };
+
+  mkParse = experimental: { _debug ? false, prefix ? "{{", postfix ? "}}", ... } @ variables: str: let
     matches = lib.match "^(.*)${fixedInMatch prefix}(.+)${fixedInMatch postfix}(.*)$" str;
     self = mkParse experimental (variables // { inherit prefix postfix;});
   in if isNull matches then str else let
     pre = lib.elemAt matches 0;
-    ctx = lib.trim (lib.elemAt matches 1);
-    rest= if experimental then
-      builtins.scopedImport variables (builtins.toFile "mkParse-expr.nix" ctx)
-    else lib.getAttrFromPath (lib.splitString "." ctx) variables;
+    c   = lib.elemAt matches 1;
+    ctx = getCtx c postfix;
+    rest=
+      if ctx.pre == "" && ctx.post == "" then
+        "${prefix}${c}${postfix}"
+      else if experimental then
+        builtins.scopedImport variables (builtins.toFile "mkParse-expr.nix" ctx.pre)
+      else lib.getAttrFromPath (lib.splitString "." ctx.pre) variables;
     post= lib.elemAt matches 2;
-  in self pre + (if lib.isStringLike rest then rest else builtins.toJSON rest) + self post;
+    res = self pre + (if lib.isStringLike rest then rest else builtins.toJSON rest) + ctx.post' + self post;
+  in if _debug then
+    lib.warn "(mkParse) found: ${prefix}${ctx.pre'}${postfix}" res
+  else res;
 in {
   inherit fromJSON fromTOML;
   fromYAML = yaml: self.readYAML (builtins.toFile "file.yaml" yaml);
