89a3f59b692fa12b82990c90fb406f8179f70fc4
fmway <fm18lv@gmail.com>
2025-06-19 08:30:23 +0700

    chore(util/mkParse): single file expr.nix to minimalize tons of files

--- lib/fmway/parser.nix
+++ lib/fmway/parser.nix
@@ -1,4 +1,4 @@
-{ lib, self, ... }: let
+{ lib, self, root, ... }: let
   inherit (lib)
     fileContents
   ;
@@ -37,9 +37,9 @@
     post = lib.trim post';
   };
 
-  mkParse = experimental: { _debug ? false, prefix ? "{{", postfix ? "}}", ... } @ variables: str: let
+  mkParse = { _debug ? false, prefix ? "{{", postfix ? "}}", ... } @ variables: str: let
     matches = lib.match "^(.*)${fixedInMatch prefix}(.+)${fixedInMatch postfix}(.*)$" str;
-    self = mkParse experimental (variables // { inherit prefix postfix;});
+    self = mkParse (variables // { inherit prefix postfix;});
   in if isNull matches then str else let
     pre = lib.elemAt matches 0;
     c   = lib.elemAt matches 1;
@@ -47,14 +47,73 @@
     rest=
       if ctx.pre == "" && ctx.post == "" then
         "${prefix}${c}${postfix}"
-      else if experimental then
-        builtins.scopedImport variables (builtins.toFile "mkParse-expr.nix" ctx.pre)
       else lib.getAttrFromPath (lib.splitString "." ctx.pre) variables;
     post= lib.elemAt matches 2;
     res = self pre + (if lib.isStringLike rest then rest else builtins.toJSON rest) + ctx.post' + self post;
-  in if _debug then
-    lib.warn "(mkParse) found: ${prefix}${ctx.pre'}${postfix}" res
-  else res;
+  in lib.warnIf _debug "(mkParse) found: ${prefix}${ctx.pre'}${postfix}" res;
+
+  fix = arr: var: let
+    text = (lib.foldl' (acc: curr: let
+      key = curr._key or "expr-${toString acc.idx}";
+      res = if lib.isString curr then curr
+        else if curr ? _let then ""
+        else obj.${key};
+    in {
+      idx = if lib.isString curr || curr ? _key || curr ? _let then acc.idx else acc.idx + 1;
+      str = acc.str + (if lib.isStringLike res then res else builtins.toJSON res);
+    }) { idx = 0; str = ""; } arr).str;
+    exprs = lib.filter (x: lib.isAttrs x && x ? _expr) arr;
+    lets = lib.filter (x: lib.isAttrs x && x ? _let) arr;
+    res = "let self = {\n" + (lib.foldl' (acc: curr: let
+      key = curr._key or "expr-${toString acc.idx}";
+    in {
+      idx = if curr ? _key then acc.idx else acc.idx + 1;
+      str = acc.str + root.addIndent "  " "${key} = ${curr._expr};\n";
+    }) { idx = 0; str = ""; } exprs).str + "}; ${lib.concatStrings (map (x: x._let) lets)} in self";
+    expr = builtins.toFile "mkParse-expr.nix" res;
+    obj = builtins.scopedImport var expr;
+  in {
+    inherit text expr;
+  };
+
+  toExpr = str: let
+    matches = lib.match "^([^=]*)=([^=].+)$" str;
+    expr = lib.trim (if isNull matches then str else lib.elemAt matches 1);
+    key = lib.trim (if isNull matches then str else lib.elemAt matches 0);
+    exprKey = if isNull matches || key != "" then "_expr" else "_let";
+  in lib.throwIf (expr == "") "(mkParse): value cannot empty in ${str}" {
+    "${exprKey}" = expr;
+  } // lib.optionalAttrs (!isNull matches && key != "") {
+    _key = key;
+  };
+
+  mkParse' = { _debug ? false, prefix ? "{{", postfix ? "}}", ... } @ variables: str: let
+    fixedPrefix = fixedInMatch prefix;
+    fixedPostfix= fixedInMatch postfix;
+    fn = res: s: 
+      if s == "" then
+        res
+      else let
+        matches = lib.match "^(.*)${fixedPrefix}(.+)${fixedPostfix}(.*)$" s;
+      in if isNull matches then
+        fn (res ++ [s]) ""
+      else let
+        pre = lib.elemAt matches 0;
+        c   = lib.elemAt matches 1;
+        ctx = getCtx c postfix;
+        rest=
+          if ctx.pre == "" && ctx.post == "" then
+            "${prefix}${c}${postfix}"
+          else toExpr ctx.pre;
+        post= lib.elemAt matches 2;
+        r   =
+          fn [] pre
+        ++lib.warnIf _debug "(mkParse) found: ${prefix}${ctx.pre'}${postfix}" [rest]
+        ++lib.optional (ctx.post' != "") ctx.post' ++ fn [] post;
+      in fn r "";
+
+    res = fix (fn [] str) variables;
+  in  lib.warnIf _debug "(mkParse) result: ${res.expr}" res.text;
 in {
   inherit fromJSON fromTOML;
   fromYAML = yaml: self.readYAML (builtins.toFile "file.yaml" yaml);
@@ -108,7 +167,7 @@ in {
     in parse "this is \${{ myvar }} and \${{ the.value.is }}" # => "this is work and work"
     ```
    */
-  mkParse = mkParse false;
+  inherit mkParse;
   # mkParse with all functionality in nix (like math operation)
-  mkParse'= mkParse true;
+  inherit mkParse';
 }
